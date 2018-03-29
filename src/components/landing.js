9068bc2937636caaeaf41525d00e8e7f4bf20c8b
evaccaro <erv230@nyu.edu>
2018-03-29 11:28:28 -0400

    first commit, just neon sign on landing page so far

---
+++ src/components/landing.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class Landing extends Component {
+  render() {
+    return (
+      <div id="Landing">
+        <div id="container">
+          <p>
+            <br />
+            <a href="https://en.wikipedia.org/wiki/Yellow">Elisa Rose</a>
+          </p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Landing;
